b4604b0a3e40341d1c5e1018b561a2da7e60734e
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-03-12 12:42:27 -0400

    calculates distance from sonar

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Dat Nguyen
+ * Created on: Mar 2026
+ * This program calculate the distance in front of the micro:bit.
 */
 
-basic.showString('Hello, World!')
+// Intialize variables
+let distance: number = 0
+
+// Intialize display
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    // Calculate distance from sonar
+    distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+
+    // Display distan                                             ce
+    basic.clearScreen()
+    basic.showString(distance.toString() + " cm")
+
+    // Reset display
+    basic.showIcon(IconNames.Happy)
+})
